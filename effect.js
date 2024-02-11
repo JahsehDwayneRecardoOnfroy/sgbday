02f9e957731c97abb2293706f9b65b1b318e88ef
Wolfie <71263340+JahsehDwayneRecardoOnfroy@users.noreply.github.com>
2024-02-11 11:07:42 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -58,49 +58,49 @@ $('document').ready(function(){
 			//loopOne();
 		//});
 	//}
-	//function loopTwo() {
-		//var randleft = 1000*Math.random();
-		//var randtop = 500*Math.random();
-		//$('#b2').animate({left:randleft,bottom:randtop},10000,function(){
-			//loopTwo();
-		//});
-	//}
-	//function loopThree() {
-		//var randleft = 1000*Math.random();
-		//var randtop = 500*Math.random();
-		//$('#b3').animate({left:randleft,bottom:randtop},10000,function(){
-			//loopThree();
-		//});
-	//}
-	//function loopFour() {
-		//var randleft = 1000*Math.random();
-		//var randtop = 500*Math.random();
-		//$('#b4').animate({left:randleft,bottom:randtop},10000,function(){
-			//loopFour();
-		//});
-	//}
-	//function loopFive() {
-		//var randleft = 1000*Math.random();
-		//var randtop = 500*Math.random();
-		//$('#b5').animate({left:randleft,bottom:randtop},10000,function(){
-			//loopFive();
-		//});
-	//}
+	function loopTwo() {
+		var randleft = 1000*Math.random();
+		var randtop = 500*Math.random();
+		$('#b2').animate({left:randleft,bottom:randtop},10000,function(){
+			loopTwo();
+		});
+	}
+	function loopThree() {
+		var randleft = 1000*Math.random();
+		var randtop = 500*Math.random();
+		$('#b3').animate({left:randleft,bottom:randtop},10000,function(){
+			loopThree();
+		});
+	}
+	function loopFour() {
+		var randleft = 1000*Math.random();
+		var randtop = 500*Math.random();
+	$('#b4').animate({left:randleft,bottom:randtop},10000,function(){
+			loopFour();
+		});
+	}
+	function loopFive() {
+		var randleft = 1000*Math.random();
+		var randtop = 500*Math.random();
+		$('#b5').animate({left:randleft,bottom:randtop},10000,function(){
+			loopFive();
+		});
+	}
 
-	//function loopSix() {
-		//var randleft = 1000*Math.random();
-		///var randtop = 500*Math.random();
-		//$('#b6').animate({left:randleft,bottom:randtop},10000,function(){
-		//	loopSix();
-		//});
-	//}
-	//function loopSeven() {
-		//var randleft = 1000*Math.random();
-		//var randtop = 500*Math.random();
-		//$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-		//	loopSeven();
-		//});
-	//}
+	function loopSix() {
+		var randleft = 1000*Math.random();
+		var randtop = 500*Math.random();
+		$('#b6').animate({left:randleft,bottom:randtop},10000,function(){
+			loopSix();
+		});
+	}
+	function loopSeven() {
+		var randleft = 1000*Math.random();
+		var randtop = 500*Math.random();
+		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
+			loopSeven();
+		});
+	}
 
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
